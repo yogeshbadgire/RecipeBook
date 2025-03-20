3d37627ba2c8bc94e1f852c6aeb635822d2479ca
Yogesh Badgire <yogeshbadgire@gmail.com>
2025-03-20 10:39:11 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -27,6 +27,66 @@ function updateRecipe() {
   for(let i = 1; i <= 50; i++) {
       cuID = "ing"+i;
       const qty = parseFloat(document.getElementById(cuID).getAttribute('value'));        
-      document.getElementById(cuID).innerHTML = qty*servings; 
+      var result = qty*servings;
+      switch(result) {
+        case 0.0625:
+            result = "1/16";
+            break;
+        case 0.125:
+            result = "1/8";
+            break;
+        case 0.1875:
+            result = "1/8 + 1/16";
+            break;
+        case 0.25:
+            result = "1/4";
+            break;
+        case 0.3125:
+            result = "1/4 + 1/16";
+            break;
+        case 0.375:
+            result = "1/4 + 1/8";
+            break;
+        case 0.4375:
+            result = "1/4 + 1/8 + 1/16";
+            break;
+        case 0.5:
+            result = "1/2";
+            break;
+        case 0.5625:
+            result = "1/2 + 1/16";
+            break;
+        case 0.625:
+            result = "1/2 + 1/8";
+            break;
+        case 0.6875:
+            result = "1/2 + 1/8 + 1/16";
+            break;
+        case 0.75:
+            result = "1/2 + 1/4";
+            break;
+        case 0.8125:
+            result = "1/2 + 1/4 + 1/16"
+            break;
+        case 0.875:
+            result = "1/2 + 1/4 + 1/8"
+            break;
+        case 0.9375:
+            result = "1/2 + 1/4 + 1/8 + 1/16"
+        default:
+            result = result;
+    }
+      document.getElementById(cuID).innerHTML = result;
   }        
-}
+}  
+
+function servingsList() {
+    const selectElement = document.getElementById("servings");
+  
+    for (let i = 3; i <= 100; i++) {
+      const option = document.createElement("option");
+      option.value = i;
+      option.text = i;
+      selectElement.add(option);
+    }
+  }
